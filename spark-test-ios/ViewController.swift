546b65291d0de8f63f556b50932ce6f28c57ec33
Ihor Buts <ihor.8uts@gmail.com>
2025-04-07 12:02:52 +0300

    add layout

--- spark-test-ios/ViewController.swift
+++ spark-test-ios/ViewController.swift
@@ -7,13 +7,58 @@
 
 import UIKit
 
+// - collection view hScroll
+// H[-collectionView-]
+// V.center
+// height 150
+
+
 class ViewController: UIViewController {
+    var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
     }
 
+    func setupUI() {
+        setupCollectionView()
+    }
 
+    func setupCollectionView() {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.itemSize = CGSize(width: 100, height: 100)
+        flowLayout.scrollDirection = .horizontal
+        flowLayout.minimumInteritemSpacing = 8
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(collectionView)
+        NSLayoutConstraint.activate([
+            NSLayoutConstraint(item: collectionView, attribute: .centerY, relatedBy: .equal, toItem: view, attribute: .centerY, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: collectionView, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leading, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: collectionView, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailing, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: collectionView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 150)
+        ])
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "reuseIdentifer")
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        self.collectionView = collectionView
+    }
+}
+
+extension ViewController: UICollectionViewDelegate {
+    
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "reuseIdentifer", for: indexPath)
+        cell.contentView.backgroundColor = .red
+        return cell
+    }
 }
 
